e85b2022b1d0bfbfd88e596e034cf4a6112f8d86
gyowoo113 <yereube@naver.com>
2023-08-18 11:08:57 +0900

    Add #3 : product table VO, Service 추가

---
+++ mealkit/src/main/java/co/yedam/mealkit/product/service/ProductVO.java
@@ -0,0 +1,14 @@
+package co.yedam.mealkit.product.service;
+
+import java.time.LocalDate;
+
+public class ProductVO {
+	private int productID;
+	private String productName;
+	private int productPrice;
+	private String productComment;
+	private LocalDate productDate;
+	private int categoryId;
+	private String productImg;
+	private int productStock;
+}
