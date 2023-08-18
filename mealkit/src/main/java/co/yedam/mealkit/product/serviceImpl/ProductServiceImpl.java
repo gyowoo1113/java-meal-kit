5daf7c87431ac8e9922fc6ccd87b77c28408d09c
gyowoo113 <yereube@naver.com>
2023-08-18 11:09:35 +0900

    Add #3 : product table DAO 추가

---
+++ mealkit/src/main/java/co/yedam/mealkit/product/serviceImpl/ProductServiceImpl.java
@@ -0,0 +1,41 @@
+package co.yedam.mealkit.product.serviceImpl;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import co.yedam.mealkit.common.DataSource;
+import co.yedam.mealkit.product.mapper.ProductMapper;
+import co.yedam.mealkit.product.service.ProductService;
+import co.yedam.mealkit.product.service.ProductVO;
+
+public class ProductServiceImpl implements ProductService {
+	SqlSession sqlSession = DataSource.getInstance().openSession(true);
+	ProductMapper map = sqlSession.getMapper(ProductMapper.class);
+	
+	@Override
+	public List<ProductVO> productSelectList() {
+		return map.productSelectList();
+	}
+
+	@Override
+	public ProductVO productSelect(ProductVO vo) {
+		return map.productSelect(vo);
+	}
+
+	@Override
+	public int productInsert(ProductVO vo) {
+		return map.productInsert(vo);
+	}
+
+	@Override
+	public int productUpdate(ProductVO vo) {
+		return map.productUpdate(vo);
+	}
+
+	@Override
+	public int productDelete(ProductVO vo) {
+		return map.productDelete(vo);
+	}
+
+}
