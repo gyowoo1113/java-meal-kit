4b58418a040ce435472eeefb5c6d02b66d48b115
gyowoo1113 <yereube@naver.com>
2023-08-20 00:21:56 +0900

    BugFix #3 : VO lombok 추가, mapper query 오류 수정

--- mealkit/src/main/java/co/yedam/mealkit/shop/cart/service/CartVO.java
+++ mealkit/src/main/java/co/yedam/mealkit/shop/cart/service/CartVO.java
@@ -1,5 +1,10 @@
 package co.yedam.mealkit.shop.cart.service;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CartVO {
 	private int cartId;
 	private int cartCount;
