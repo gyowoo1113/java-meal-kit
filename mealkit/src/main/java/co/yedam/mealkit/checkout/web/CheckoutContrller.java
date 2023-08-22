fc129f62d1c8b21744d1c571490919a919ba7517
gyowoo113 <yereube@naver.com>
2023-08-22 11:40:47 +0900

    Feat #2 : 장바구니의 물품 id값 받아와서 List 생성

--- mealkit/src/main/java/co/yedam/mealkit/checkout/web/CheckoutContrller.java
+++ mealkit/src/main/java/co/yedam/mealkit/checkout/web/CheckoutContrller.java
@@ -1,6 +1,10 @@
 package co.yedam.mealkit.checkout.web;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,6 +22,14 @@ public class CheckoutContrller extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int listNum = Integer.valueOf(request.getParameter("list_num"));
+		
+		List<Integer> ids = new ArrayList<Integer>();
+		for (int i=0; i<listNum; ++i) {
+			String name = "carts[" + i + "].cartId";
+			int cartId = Integer.valueOf(request.getParameter(name));
+			ids.add(cartId);
+		}
 		String viewName = "checkout/checkout";
 		ViewResolve.forward(request, response, viewName);
 	}
