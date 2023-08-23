2e2641d44341f4f5168add69cd2f45e1af57666e
gyowoo113 <yereube@naver.com>
2023-08-23 10:26:51 +0900

    Refactor #2 : 장바구니 목록 setting하는 내용 method 추출

--- mealkit/src/main/java/co/yedam/mealkit/checkout/web/CheckoutContrller.java
+++ mealkit/src/main/java/co/yedam/mealkit/checkout/web/CheckoutContrller.java
@@ -10,7 +10,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import co.yedam.mealkit.common.ViewResolve;
@@ -26,6 +28,16 @@ public class CheckoutContrller extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		setCartList(request);
+		String viewName = "checkout/checkout";
+		ViewResolve.forward(request, response, viewName);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
+	protected void setCartList(HttpServletRequest request) throws JsonProcessingException {
 		int listNum = Integer.valueOf(request.getParameter("list_num"));
 		
 		List<Integer> ids = new ArrayList<Integer>();
@@ -44,13 +56,6 @@ public class CheckoutContrller extends HttpServlet {
 		carts = dao.cartSelectInList(ids);
 		String data = objectMapper.writeValueAsString(carts);
 		request.setAttribute("carts", data);
-		
-		String viewName = "checkout/checkout";
-		ViewResolve.forward(request, response, viewName);
 	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
-
+	
 }
