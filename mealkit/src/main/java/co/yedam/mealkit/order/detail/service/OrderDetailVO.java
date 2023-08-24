4b30c5f0f76ad3fadd8f247cd1d3d22de037ffb4
gyowoo113 <yereube@naver.com>
2023-08-24 09:05:39 +0900

    Feat #3 : order_detail field 변경, add mapper and constructor

--- mealkit/src/main/java/co/yedam/mealkit/order/detail/service/OrderDetailVO.java
+++ mealkit/src/main/java/co/yedam/mealkit/order/detail/service/OrderDetailVO.java
@@ -4,5 +4,14 @@ public class OrderDetailVO {
 	private int orderDetailCount;
 	private int orderDetailPrice;
 	private int productId;
-	private String orderCode;
+	private int orderId;
+	
+	public OrderDetailVO() {}
+	public OrderDetailVO(int orderDetailCount, int orderDetailPrice, int productId, int orderId) {
+		super();
+		this.orderDetailCount = orderDetailCount;
+		this.orderDetailPrice = orderDetailPrice;
+		this.productId = productId;
+		this.orderId = orderId;
+	}
 }
