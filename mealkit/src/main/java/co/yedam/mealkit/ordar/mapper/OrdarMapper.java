f2a0d7d4e7a8c0899d737321f112ed15d5fee34d
gyowoo113 <yereube@naver.com>
2023-08-24 11:56:10 +0900

    Feat #3 : 해당 멤버의 주문(ordar) 데이터 List select query 추가

--- mealkit/src/main/java/co/yedam/mealkit/ordar/mapper/OrdarMapper.java
+++ mealkit/src/main/java/co/yedam/mealkit/ordar/mapper/OrdarMapper.java
@@ -1,11 +1,13 @@
 package co.yedam.mealkit.ordar.mapper;
 
 import java.util.List;
+import java.util.Map;
 
 import co.yedam.mealkit.ordar.service.OrdarVO;
 
 public interface OrdarMapper {
 	List<OrdarVO> ordarSelectList();
+	List<OrdarVO> ordarSelectList(String memberId);
 	OrdarVO ordarSelect(OrdarVO vo);
 	int ordarInsert(OrdarVO vo);
 	int ordarUpdate(OrdarVO vo);
--- mealkit/src/main/java/co/yedam/mealkit/ordar/service/OrdarService.java
+++ mealkit/src/main/java/co/yedam/mealkit/ordar/service/OrdarService.java
@@ -1,9 +1,11 @@
 package co.yedam.mealkit.ordar.service;
 
 import java.util.List;
+import java.util.Map;
 
 public interface OrdarService {
 	List<OrdarVO> ordarSelectList();
+	List<OrdarVO> ordarSelectList(String memberId);
 	OrdarVO ordarSelect(OrdarVO vo);
 	int ordarInsert(OrdarVO vo);
 	int ordarUpdate(OrdarVO vo);
